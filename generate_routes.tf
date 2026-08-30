633fb13798a19f7f3069eda8c47daa4c177f12be
Jude Quintana <jude.quintana@gmail.com>
2026-08-29 18:46:20 -0600

    test

--- generate_routes.tf
+++ generate_routes.tf
@@ -8,8 +8,9 @@ locals {
 }
 
 module "this_generate_routes_to_other_vpcs" {
-  source  = "JudeQuintana/generate-routes-to-other-vpcs/aws"
-  version = "1.2.0"
+  #source  = "JudeQuintana/generate-routes-to-other-vpcs/aws"
+  #version = "1.2.0"
+  source = "git@github.com:JudeQuintana/terraform-aws-generate-routes-to-other-vpcs.git?ref=reachability-provenance"
 
   generate_routes_to_other_vpcs = {
     routing_policy            = var.full_mesh_trio.routing_policy
